6550fb04be46be217f76c9027b4bb9d9156c79e1
unknown <Glas@.(none)>
2010-10-01 17:24:40 -0500

    deleted garbage

--- typenums.h
+++
@@ -1,6 +0,0 @@
-
-//typedef unsigned short UInt8;	typedef short SInt8;
-typedef unsigned short UInt16;	typedef short SInt16;
-typedef unsigned UInt32;		typedef int SInt32;
-typedef unsigned long UInt64;	typedef long SInt64;
-//need to add a notation for float
