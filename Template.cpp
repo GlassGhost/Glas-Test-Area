8e555ad03b197b6c9712f69b4be6142a774fa9f5
unknown <Glas@.(none)>
2010-10-03 16:23:12 -0500

    update license

--- Template.cpp
+++ Template.cpp
@@ -1,28 +1,34 @@
 /**********************************License(s)***********************************
-Copyright (c) 2010, Roy Pfund.                              All rights reserved.
+Copyright 2010 Roy Pfund.                                   All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright  notice,  this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce  the  above  copyright  notice,
+   this list of conditions and the following  disclaimer  in  the  documentation
+   and/or other materials provided with the distribution.
+3. All advertising materials mentioning features or use of  this  software  must
+   display the following acknowledgement:
+This product includes software developed by Roy Pfund and its contributors.
+4. Neither the name of Roy Pfund nor the names of its contributors may  be  used
+   to endorse or promote products derived from this  software  without  specific
+   prior written permission.
 
- 1. Redistributions of source code must retain the above copyright notice,  this
-    list of conditions and the following disclaimer.
- 2. Redistributions in binary form  must reproduce  the above  copyright notice,
-    this  list of conditions  and  the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
- 3. Neither the name of the Copyright Holders  nor the names of its Contributors
-    may  be  used  to endorse  or  promote products  derived from  this software
-    without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND  CONTRIBUTORS  "AS  IS"
+AND ANY EXPRESS OR IMPLIED  WARRANTIES,  INCLUDING,  BUT  NOT  LIMITED  TO,  THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR  PURPOSE  ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) OR CONTRIBUTORS BE  LIABLE
+FOR ANY DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED  TO,  PROCUREMENT  OF  SUBSTITUTE  GOODS  OR
+SERVICES; LOSS OF USE, DATA,  OR  PROFITS;  OR  BUSINESS  INTERRUPTION)  HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF  THE  USE  OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. --4 Clause BSD
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  AND
-ANY EXPRESS OR IMPLIED WARRANTIES,  INCLUDING,  BUT NOT LIMITED TO,  THE IMPLIED
-WARRANTIES  OF  MERCHANTABILITY  AND  FITNESS  FOR  A  PARTICULAR  PURPOSE   ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNERS OR CONTRIBUTORS BE LIABLE FOR
-ANY  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,  OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;LOSS
-OF USE, DATA,  OR PROFITS;  OR BUSINESS INTERRUPTION)  HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY,  WHETHER IN CONTRACT, STRICT LIABILITY,  OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE)   ARISING IN ANY WAY  OUT OF THE USE OF  THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.                - 3 Clause BSD
+The views and conclusions contained in the software and documentation are  those
+of the authors and should not be interpreted as representing official  policies,
+either expressed or implied, of Roy Pfund.
 *************************************Inputs*************************************
 accepts a command line arg "-kill"
 *********************************Pre-Conditions*********************************
@@ -48,12 +54,14 @@ prints my own version of "Hello World"
 typedef unsigned short UINT16; typedef short	INT16;
 typedef unsigned	   UINT32; typedef int		INT32; typedef float  FLOAT32;
 typedef unsigned long  UINT64; typedef long int INT64; typedef double FLOAT64;
-/*****************************Feature Test Switches****************************/
-#define _POSIX_SOURCE 0
 /************************************Macros************************************/
-#define kill_arg_bool ((std::bitset<16>) *program_flagsUINT16Ptr)[0]
+#define kill_arg_bool ((std::bitset<16>)*program_flagsUINT16Ptr)[0]
 /**********************************Variables***********************************/
 UINT16* program_flagsUINT16Ptr = new UINT16(0);
+/*****************************Feature Test Switches****************************/
+/*hopefully someday I will be able to define this with 1*/
+#define _POSIX_SOURCE 0
+kill_arg_bool = 1;
 /*****************************File scope variables*****************************/
 /******************************External variables******************************/
 /*****************************Structures and Unions****************************/
