b3536df60776362bd8b188f0314dc619f7b05e30
GlassGhost <RoyPfund@gmail.com>
2010-10-05 15:29:33 -0500

    update

--- Template.cpp
+++ Template.cpp
@@ -1,34 +1,14 @@
 /**********************************License(s)***********************************
-Copyright © 2010 Roy Pfund.                                 All rights reserved.
+Copyright 2010 Roy Pfund.                                   All rights reserved.
+Use of this source code is governed by a BSD-style License(the "License");  that
+can be found in the LICENSE file.  You may obtain another copy of the License at
 
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright  notice,  this
-   list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce  the  above  copyright  notice,
-   this list of conditions and the following  disclaimer  in  the  documentation
-   and/or other materials provided with the distribution.
-3. All advertising materials mentioning features or use of  this  software  must
-   display the following acknowledgement:
-This product includes software developed by Roy Pfund and its contributors.
-4. Neither the name of the Copyright Holder(s) nor the names of its contributors
-   may be used to endorse or promote products derived from this software without
-   specific prior written permission.
+    http://github.com/GlassGhost/Glas-Test-Area/raw/master/LICENSE.txt
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S)  AND  CONTRIBUTORS  "AS IS"
-AND  ANY  EXPRESS OR IMPLIED  WARRANTIES,  INCLUDING,  BUT NOT  LIMITED TO,  THE
-IMPLIED WARRANTIES OF MERCHANTABILITY  AND  FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) OR CONTRIBUTORS  BE LIABLE
-FOR ANY DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED  TO,  PROCUREMENT  OF  SUBSTITUTE  GOODS  OR
-SERVICES;  LOSS OF USE,  DATA,  OR PROFITS;  OR  BUSINESS INTERRUPTION)  HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN  ANY  WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. --4 Clause BSD
-
-The views and conclusions contained in the software and documentation are  those
-of the authors and should not be interpreted as representing official  policies,
-either expressed or implied, of Roy Pfund.
+Unless required by applicable law or agreed to in writing, software  distributed
+under the License is distributed on an "AS  IS"  BASIS,  WITHOUT  WARRANTIES  OR
+CONDITIONS OF ANY KIND, either express or  implied.  See  the  License  for  the
+specific language governing permissions and limitations under the License.
 *************************************Inputs*************************************
 accepts a command line arg "-kill"
 *********************************Pre-Conditions*********************************
@@ -66,7 +46,6 @@ kill_arg_bool = 1;
 /******************************External variables******************************/
 /*****************************Structures and Unions****************************/
 /**********************************Functions***********************************/
-
 void kill(){
 /*Input(s)		:nothing
 PreCondition(s) :none
